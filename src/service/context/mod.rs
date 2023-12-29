e907e1a47e3d01ee22cd9578902db3c53bce4bad
glendc <glen.decauwsemaecker@otainsight.com>
2023-12-29 23:03:15 +0100

    add spawn method to context
    e.g. which will also be used for h2 conn pollers

--- src/service/context/mod.rs
+++ src/service/context/mod.rs
@@ -1,7 +1,11 @@
 //! Context passed to and between services as input.
 
+use std::future::Future;
+use tokio::task::JoinHandle;
+
 mod extensions;
 pub use extensions::Extensions;
+use tokio_graceful::ShutdownGuard;
 
 /// Context passed to and between services as input.
 #[derive(Debug, Clone)]
@@ -43,5 +47,20 @@ impl<S> Context<S> {
     /// Get a mutable reference to the extensions.
     pub fn extensions_mut(&mut self) -> &mut Extensions {
         &mut self.extensions
+    }
+}
+
+impl<S> Context<S> {
+    /// Spawn a future on the current executor,
+    /// this is spawned gracefully in case a shutdown guard has been registered.
+    pub fn spawn<F>(&self, future: F) -> JoinHandle<F::Output>
+    where
+        F: Future + Send + 'static,
+        F::Output: Send + 'static,
+    {
+        match self.extensions.get::<ShutdownGuard>() {
+            Some(guard) => guard.spawn_task(future),
+            None => tokio::spawn(future),
+        }
     }
 }
