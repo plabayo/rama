1819179f89b7d2986e150fd813c718b613a2066a
glendc <glen.decauwsemaecker@otainsight.com>
2024-01-03 10:37:47 +0100

    add http types and headers reexports

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -10,3 +10,20 @@ pub type Request<T = Body> = http::Request<T>;
 /// Type alias for [`http::Response`] whose body type
 /// defaults to [`Body`], the most common body type used with rama.
 pub type Response<T = Body> = http::Response<T>;
+
+pub mod headers;
+pub mod utils;
+
+pub use http::header::HeaderMap;
+pub use http::header::HeaderName;
+pub use http::header::HeaderValue;
+pub use http::method::Method;
+pub use http::status::StatusCode;
+pub use http::uri::Uri;
+pub use http::version::Version;
+
+pub mod dep {
+    //! Dependencies for rama http modules.
+    pub use http;
+    pub use http_body;
+}
---
+++ src/http/utils/mod.rs
@@ -0,0 +1,4 @@
+//! Utilities for HTTP.
+
+mod header_value;
+pub use header_value::{HeaderValueErr, HeaderValueGetter};
