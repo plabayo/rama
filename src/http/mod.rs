2c428950f8914a3712f9f337c76ef07488201295
glendc <glen.decauwsemaecker@otainsight.com>
2024-01-03 10:25:21 +0100

    add request/response http type aliases

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,3 +2,11 @@
 
 pub(crate) mod body;
 pub use body::{Body, BodyDataStream};
+
+/// Type alias for [`http::Request`] whose body type
+/// defaults to [`Body`], the most common body type used with rama.
+pub type Request<T = Body> = http::Request<T>;
+
+/// Type alias for [`http::Response`] whose body type
+/// defaults to [`Body`], the most common body type used with rama.
+pub type Response<T = Body> = http::Response<T>;
