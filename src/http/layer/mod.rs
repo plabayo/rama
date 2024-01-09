9ba25115279a07fa7dcca08f03efc1a39d82ee98
glendc <glen.decauwsemaecker@otainsight.com>
2024-01-09 13:08:07 +0100

    port set_status http layer from tower-async

--- src/http/layer/mod.rs
+++ src/http/layer/mod.rs
@@ -2,4 +2,5 @@
 
 pub mod dns;
 pub mod header_config;
+pub mod set_status;
 pub mod validate_request;
---
+++ src/http/layer/set_status.rs
@@ -0,0 +1,108 @@
+//! Middleware to override status codes.
+//!
+//! # Example
+//!
+//! ```
+//! use std::{iter::once, convert::Infallible};
+//! use bytes::Bytes;
+//! use rama::http::layer::set_status::SetStatusLayer;
+//! use rama::http::{Body, Request, Response, StatusCode};
+//! use rama::service::{Context, ServiceBuilder, Service};
+//! use rama::error::Error;
+//!
+//! async fn handle(req: Request) -> Result<Response, Infallible> {
+//!     // ...
+//!     # Ok(Response::new(Body::empty()))
+//! }
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Error> {
+//! let mut service = ServiceBuilder::new()
+//!     // change the status to `404 Not Found` regardless what the inner service returns
+//!     .layer(SetStatusLayer::new(StatusCode::NOT_FOUND))
+//!     .service_fn(handle);
+//!
+//! // Call the service.
+//! let request = Request::builder().body(Body::empty())?;
+//!
+//! let response = service.serve(Context::default(), request).await?;
+//!
+//! assert_eq!(response.status(), StatusCode::NOT_FOUND);
+//! #
+//! # Ok(())
+//! # }
+//! ```
+
+use crate::http::{Request, Response, StatusCode};
+use crate::service::{Context, Layer, Service};
+
+/// Layer that applies [`SetStatus`] which overrides the status codes.
+#[derive(Debug, Clone, Copy)]
+pub struct SetStatusLayer {
+    status: StatusCode,
+}
+
+impl SetStatusLayer {
+    /// Create a new [`SetStatusLayer`].
+    ///
+    /// The response status code will be `status` regardless of what the inner service returns.
+    pub fn new(status: StatusCode) -> Self {
+        SetStatusLayer { status }
+    }
+}
+
+impl<S> Layer<S> for SetStatusLayer {
+    type Service = SetStatus<S>;
+
+    fn layer(&self, inner: S) -> Self::Service {
+        SetStatus::new(inner, self.status)
+    }
+}
+
+/// Middleware to override status codes.
+///
+/// See the [module docs](self) for more details.
+#[derive(Debug, Clone, Copy)]
+pub struct SetStatus<S> {
+    inner: S,
+    status: StatusCode,
+}
+
+impl<S> SetStatus<S> {
+    /// Create a new [`SetStatus`].
+    ///
+    /// The response status code will be `status` regardless of what the inner service returns.
+    pub fn new(inner: S, status: StatusCode) -> Self {
+        Self { status, inner }
+    }
+
+    define_inner_service_accessors!();
+
+    /// Returns a new [`Layer`] that wraps services with a `SetStatus` middleware.
+    ///
+    /// [`Layer`]: crate::service::Layer
+    pub fn layer(status: StatusCode) -> SetStatusLayer {
+        SetStatusLayer::new(status)
+    }
+}
+
+impl<State, S, ReqBody, ResBody> Service<State, Request<ReqBody>> for SetStatus<S>
+where
+    State: Send + Sync + 'static,
+    S: Service<State, Request<ReqBody>, Response = Response<ResBody>>,
+    ReqBody: Send + 'static,
+    ResBody: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+
+    async fn serve(
+        &self,
+        ctx: Context<State>,
+        req: Request<ReqBody>,
+    ) -> Result<Self::Response, Self::Error> {
+        let mut response = self.inner.serve(ctx, req).await?;
+        *response.status_mut() = self.status;
+        Ok(response)
+    }
+}
