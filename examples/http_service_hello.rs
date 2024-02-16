cef08fb84dffa936e883dc34907435f3c3786b4e
glendc <glen.decauwsemaecker@otainsight.com>
2024-02-16 10:44:28 +0100

    add more example layer code to the http_service_hello example

--- examples/http_service_hello.rs
+++ examples/http_service_hello.rs
@@ -1,12 +1,25 @@
+use bytes::Bytes;
 use rama::{
-    http::response::Html,
-    http::{server::HttpServer, Request},
+    http::{
+        header,
+        layer::{
+            compression::CompressionLayer,
+            sensitive_headers::{
+                SetSensitiveRequestHeadersLayer, SetSensitiveResponseHeadersLayer,
+            },
+            trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer},
+        },
+        response::Html,
+        server::HttpServer,
+        IntoResponse, Request,
+    },
+    latency::LatencyUnit,
     rt::Executor,
-    service::{layer::TimeoutLayer, service_fn, Context, ServiceBuilder},
+    service::{layer::TimeoutLayer, Context, ServiceBuilder},
     stream::layer::{BytesRWTrackerHandle, BytesTrackerLayer},
     tcp::server::{TcpListener, TcpSocketInfo},
 };
-use std::time::Duration;
+use std::{sync::Arc, time::Duration};
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
@@ -23,8 +36,54 @@ async fn main() {
 
     let graceful = rama::graceful::Shutdown::default();
 
-    graceful.spawn_task_fn(|guard| async {
+    let sensitive_headers: Arc<[_]> = vec![header::AUTHORIZATION, header::COOKIE].into();
+
+    graceful.spawn_task_fn(|guard| async move {
         let exec = Executor::graceful(guard.clone());
+
+        let http_service = ServiceBuilder::new()
+            .layer(CompressionLayer::new())
+            .layer(SetSensitiveRequestHeadersLayer::from_shared(sensitive_headers.clone()))
+            .layer(
+                TraceLayer::new_for_http()
+                .on_body_chunk(|chunk: &Bytes, latency: Duration, _: &tracing::Span| {
+                    tracing::trace!(size_bytes = chunk.len(), latency = ?latency, "sending body chunk")
+                })
+                .make_span_with(DefaultMakeSpan::new().include_headers(true))
+                .on_response(DefaultOnResponse::new().include_headers(true).latency_unit(LatencyUnit::Micros)),
+            )
+            .layer(SetSensitiveResponseHeadersLayer::from_shared(sensitive_headers))
+            .map_response(IntoResponse::into_response)
+            .service_fn(
+                |ctx: Context<()>, req: Request| async move {
+                    let socket_info = ctx.get::<TcpSocketInfo>().unwrap();
+                    let tracker = ctx.get::<BytesRWTrackerHandle>().unwrap();
+                    Ok(Html(format!(
+                        r##"
+                        <html>
+                            <head>
+                                <title>Rama — Http Service Hello</title>
+                            </head>
+                            <body>
+                                <h1>Hello</h1>
+                                <p>Peer: {}</p>
+                                <p>Path: {}</p>
+                                <p>Stats (bytes):</p>
+                                <ul>
+                                    <li>Read: {}</li>
+                                    <li>Written: {}</li>
+                                </ul>
+                            </body>
+                        </html>"##,
+                        socket_info.peer_addr(),
+                        req.uri().path(),
+                        tracker.read(),
+                        tracker.written(),
+                    )))
+                },
+            );
+
+        let tcp_http_service = HttpServer::auto(exec).service(http_service);
 
         TcpListener::bind("127.0.0.1:8080")
             .await
@@ -35,34 +94,7 @@ async fn main() {
                     .trace_err()
                     .layer(TimeoutLayer::new(Duration::from_secs(8)))
                     .layer(BytesTrackerLayer::new())
-                    .service(HttpServer::auto(exec).service(service_fn(
-                        |ctx: Context<()>, req: Request| async move {
-                            let socket_info = ctx.get::<TcpSocketInfo>().unwrap();
-                            let tracker = ctx.get::<BytesRWTrackerHandle>().unwrap();
-                            Ok(Html(format!(
-                                r##"
-                                <html>
-                                    <head>
-                                        <title>Rama — Http Service Hello</title>
-                                    </head>
-                                    <body>
-                                        <h1>Hello</h1>
-                                        <p>Peer: {}</p>
-                                        <p>Path: {}</p>
-                                        <p>Stats (bytes):</p>
-                                        <ul>
-                                            <li>Read: {}</li>
-                                            <li>Written: {}</li>
-                                        </ul>
-                                    </body>
-                                </html>"##,
-                                socket_info.peer_addr(),
-                                req.uri().path(),
-                                tracker.read(),
-                                tracker.written(),
-                            )))
-                        },
-                    ))),
+                    .service(tcp_http_service),
             )
             .await;
     });
