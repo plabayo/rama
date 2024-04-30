1b130318850de19655fb6f05f2673b51343ddc40
glendc <glen.decauwsemaecker@otainsight.com>
2024-04-30 22:52:15 +0200

    bugfix Fuse future

--- src/future.rs
+++ src/future.rs
@@ -27,19 +27,12 @@ impl<Fut: Future> Future for Fuse<Fut> {
     type Output = Fut::Output;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Fut::Output> {
-        // safety: we use this &mut only for matching, not for movement
-        let v = match self.as_mut().project().future.as_pin_mut() {
-            Some(fut) => {
-                // safety: this re-pinned future will never move before being dropped
-                match fut.poll(cx) {
-                    Poll::Pending => return Poll::Pending,
-                    Poll::Ready(v) => v,
-                }
-            }
-            None => return Poll::Pending,
-        };
-
-        self.as_mut().project().future.as_pin_mut().take();
-        Poll::Ready(v)
+        match self.as_mut().project().future.as_pin_mut() {
+            Some(fut) => fut.poll(cx).map(|output| {
+                self.project().future.set(None);
+                output
+            }),
+            None => Poll::Pending,
+        }
     }
 }
