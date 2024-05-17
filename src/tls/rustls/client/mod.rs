2ee0fd974fd42ec386b87b06caf4188094cb2d83
glendc <glen.decauwsemaecker@otainsight.com>
2024-05-17 22:54:20 +0200

    remove no longer used tls client service

--- src/tls/rustls/client/mod.rs
+++ src/tls/rustls/client/mod.rs
@@ -1,9 +1,5 @@
 //! TLS client support for Rama.
 
-mod service;
-#[doc(inline)]
-pub use service::{TlsConnectError, TlsConnectService};
-
 mod http;
 #[doc(inline)]
 pub use http::{AutoTlsStream, HttpsConnector, HttpsConnectorLayer};
