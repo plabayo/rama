46347cb584d005b1f505be03464f2bf9eda28bbc
glendc <glen.decauwsemaecker@otainsight.com>
2024-05-19 00:08:08 +0200

    clarify why there is no explicit http server proxy support

--- src/proxy/http/mod.rs
+++ src/proxy/http/mod.rs
@@ -1,5 +1,14 @@
 //! Http Proxy Support
 //!
 //! As defined in <https://www.ietf.org/rfc/rfc2068.txt>.
+//!
+//! Client side lives in the [`client`] module.
+//!
+//! There is no explicit server side support for HTTP Proxies,
+//! as this is achieved by using the [`HttpServer`] in combination
+//! with the upgrade mechanism to establish a tunnel for https
+//! or forwarding the request to the target server for http.
+//!
+//! [`HttpServer`]: crate::http::server::HttpServer
 
 pub mod client;
