337e359f0a29b74da9fe5e8eab50716c29ddbe13
glendc <contact@glendc.com>
2024-09-19 22:46:44 +0200

    drop sanatize logic for client svc... might give more trouble than what it saves us

--- rama-http-backend/src/client/svc.rs
+++ rama-http-backend/src/client/svc.rs
@@ -2,11 +2,7 @@ use rama_core::{
     error::{BoxError, OpaqueError},
     Context, Service,
 };
-use rama_http_types::{
-    dep::http::uri::PathAndQuery, dep::http_body, header::HOST, headers::HeaderMapExt, Method,
-    Request, Response, Version,
-};
-use rama_net::{address::ProxyAddress, http::RequestContext};
+use rama_http_types::{dep::http_body, Method, Request, Response};
 use tokio::sync::Mutex;
 
 #[derive(Debug)]
@@ -54,7 +50,7 @@ where
 }
 
 fn sanitize_client_req_header<S, B>(
-    ctx: &mut Context<S>,
+    _ctx: &mut Context<S>,
     req: Request<B>,
 ) -> Result<Request<B>, BoxError> {
     Ok(match req.method() {
@@ -65,42 +61,6 @@ fn sanitize_client_req_header<S, B>(
             }
             req
         }
-        _ => {
-            // GET | HEAD | POST | PUT | DELETE | OPTIONS | TRACE | PATCH
-            if !ctx.contains::<ProxyAddress>()
-                && req.uri().host().is_some()
-                && req.version() <= Version::HTTP_11
-            {
-                // ensure request context is defined prior to doing this, as otherwise we can get issues
-                let _ = ctx.get_or_try_insert_with_ctx::<RequestContext, _>(|ctx| {
-                    (ctx, &req).try_into()
-                })?;
-
-                tracing::trace!(
-                    "remove authority and scheme from non-connect direct http(~1) request"
-                );
-                let (mut parts, body) = req.into_parts();
-                let mut uri_parts = parts.uri.into_parts();
-                uri_parts.scheme = None;
-                let authority = uri_parts
-                    .authority
-                    .take()
-                    .expect("to exist due to our host existence test");
-                if uri_parts.path_and_query.as_ref().map(|pq| pq.as_str()) == Some("/") {
-                    uri_parts.path_and_query = Some(PathAndQuery::from_static("/"));
-                }
-
-                if !parts.headers.contains_key(HOST) {
-                    parts
-                        .headers
-                        .typed_insert(rama_http_types::headers::Host::from(authority));
-                }
-
-                parts.uri = rama_http_types::Uri::from_parts(uri_parts)?;
-                Request::from_parts(parts, body)
-            } else {
-                req
-            }
-        }
+        _ => req,
     })
 }
