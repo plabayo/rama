a8fdf10902485ed68272d199485da3aadd16a93f
Glen De Cauwsemaecker <contact@glendc.com>
2024-12-29 23:37:14 +0100

    remove atty from h2 support crate (gh reported vuln)

--- rama-http-core/tests/h2-support/src/trace.rs
+++ rama-http-core/tests/h2-support/src/trace.rs
@@ -1,11 +1,12 @@
 pub use tracing;
 pub use tracing_subscriber;
 
+use std::io::{self, IsTerminal};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
 pub fn init() -> tracing::dispatcher::DefaultGuard {
-    let use_colors = atty::is(atty::Stream::Stdout);
+    let use_colors = io::stdout().is_terminal();
     let layer = tracing_tree::HierarchicalLayer::default()
         .with_writer(tracing_subscriber::fmt::writer::TestWriter::default())
         .with_indent_lines(true)
