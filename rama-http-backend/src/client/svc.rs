355bfa45c652806ed6dcff652eb9fe2ff28c44fb
Glen De Cauwsemaecker <contact@glendc.com>
2025-01-20 10:03:34 +0100

    make it a warning if h3 is passing through header sanitizer

--- rama-http-backend/src/client/svc.rs
+++ rama-http-backend/src/client/svc.rs
@@ -179,7 +179,7 @@ fn sanitize_client_req_header<S, B>(
             req
         }
         Version::HTTP_3 => {
-            tracing::debug!(
+            tracing::warn!(
                 uri = %req.uri(),
                 "h3 request detected, but sanitize_client_req_header does not yet support this",
             );
