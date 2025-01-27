f280156b56373ab833c3789b5b6daa76545652a0
hafihaf123 <misobuchta@gmail.com>
2025-01-27 23:34:40 +0100

    enhance code, add valid_date.rs

--- rama-http/src/headers/x_robots_tag/custom_rule.rs
+++ rama-http/src/headers/x_robots_tag/custom_rule.rs
@@ -1,31 +1,31 @@
 use crate::headers::util::value_string::{FromStrError, HeaderValueString};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct CustomRule {
+pub(super) struct CustomRule {
     key: HeaderValueString,
     value: Option<HeaderValueString>,
 }
 
 impl CustomRule {
-    pub fn new(key: &str) -> Result<Self, FromStrError> {
+    pub(super) fn new(key: &str) -> Result<Self, FromStrError> {
         Ok(Self {
             key: key.parse()?,
             value: None,
         })
     }
 
-    pub fn with_value(key: &str, value: &str) -> Result<Self, FromStrError> {
+    pub(super) fn with_value(key: &str, value: &str) -> Result<Self, FromStrError> {
         Ok(Self {
             key: key.parse()?,
             value: Some(value.parse()?),
         })
     }
 
-    pub fn key(&self) -> &HeaderValueString {
+    pub(super) fn key(&self) -> &HeaderValueString {
         &self.key
     }
 
-    pub fn value(&self) -> Option<&HeaderValueString> {
+    pub(super) fn value(&self) -> Option<&HeaderValueString> {
         self.value.as_ref()
     }
 }
