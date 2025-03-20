c3c5a45d52863908bdfcf5fe7b0e2336dca9a664
brecht stamper <stamper.brecht@gmail.com>
2025-03-20 13:00:07 +0100

    delete rustls in rama-tls

--- rama-tls/src/rustls/client/mod.rs
+++
@@ -1,9 +0,0 @@
-//! TLS client support for Rama.
-
-mod connector;
-#[doc(inline)]
-pub use connector::{AutoTlsStream, TlsConnector, TlsConnectorLayer};
-
-mod connector_data;
-#[doc(inline)]
-pub use connector_data::TlsConnectorData;
