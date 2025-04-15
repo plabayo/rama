plabayo/rama

commit 4585e25dba417d806eb34e19d4f75194bbc6b633

strip default ports from authority header in h2 (#506)
Browsers by default strip port from authority header if port is the default port for that specific protocol.
Some reverse proxies (eg nginx with some configs) do exact matches on authority header and return 404, if it doesn't match. Meaning authority header `example.com:443` will be seen as invalid and only `example.com` will be accepted.

PR https://github.com/plabayo/rama/pull/506

rama-net/src/address/proxy.rs
@@ -41,7 +41,10 @@ impl TryFrom<&str> for ProxyAddress {
                         Host::try_from(&slice[i + 1..]).map(|h| {
                             (
                                 h,
-                                protocol.as_ref().unwrap_or(&Protocol::HTTP).default_port(),
+                                protocol
+                                    .as_ref()
+                                    .and_then(|proto| proto.default_port())
+                                    .unwrap_or(80),
                             )
                                 .into()
                         })
@@ -62,7 +65,10 @@ impl TryFrom<&str> for ProxyAddress {
                 Host::try_from(slice).map(|h| {
                     (
                         h,
-                        protocol.as_ref().unwrap_or(&Protocol::HTTP).default_port(),
+                        protocol
+                            .as_ref()
+                            .and_then(|proto| proto.default_port())
+                            .unwrap_or(80),
                     )
                         .into()
                 })

rama-http-backend/src/client/svc.rs
@@ -239,10 +239,17 @@ fn sanitize_client_req_header<S, B>(
                 // NOTE: in a green future we might not need to stringify
                 // this entire thing first... maybe something someone at some
                 // point can take a look at this mess
+
+                // Default port is stripped in browsers. It's important that we also do this
+                // as some reverse proxies such as nginx respond 404 if authority is not an exact match
+                let authority = if request_ctx.authority_has_default_port() {
+                    request_ctx.authority.host().to_string()
+                } else {
+                    request_ctx.authority.to_string()
+                };
+
                 uri_parts.authority = Some(
-                    request_ctx
-                        .authority
-                        .to_string()
+                    authority
                         .try_into()
                         .context("use RequestContext.authority as http authority")?,
                 );

rama-net/src/http/request_context.rs
@@ -52,6 +52,13 @@ pub struct RequestContext {
     pub authority: Authority,
 }
 
+impl RequestContext {
+    /// Check if [`Authority`] is using the default port for the [`Protocol`] set in this [`RequestContext`]
+    pub fn authority_has_default_port(&self) -> bool {
+        self.protocol.default_port() == Some(self.authority.port())
+    }
+}
+
 impl<Body, State> TryFrom<(&Context<State>, &Request<Body>)> for RequestContext {
     type Error = OpaqueError;
 
@@ -64,7 +71,9 @@ impl<Body, State> TryFrom<(&Context<State>, &Request<Body>)> for RequestContext 
             uri.scheme()
         );
 
-        let default_port = uri.port_u16().unwrap_or_else(|| protocol.default_port());
+        let default_port = uri
+            .port_u16()
+            .unwrap_or_else(|| protocol.default_port().unwrap_or(80));
         tracing::trace!(uri = %uri, "request context: detected default port: {default_port}");
 
         let authority = match ctx.get().and_then(try_get_host_from_secure_transport) {
@@ -141,7 +150,9 @@ impl<State> TryFrom<(&Context<State>, &Parts)> for RequestContext {
             uri.scheme()
         );
 
-        let default_port = uri.port_u16().unwrap_or_else(|| protocol.default_port());
+        let default_port = uri
+            .port_u16()
+            .unwrap_or_else(|| protocol.default_port().unwrap_or(80));
         tracing::trace!(uri = %uri, "request context: detected default port: {default_port}");
 
         let authority = match ctx.get().and_then(try_get_host_from_secure_transport) {
@@ -485,7 +496,10 @@ mod tests {
             assert_eq!(req_ctx.protocol, expected_protocol);
             assert_eq!(
                 req_ctx.authority.to_string(),
-                format!("www.example.com:{}", expected_protocol.default_port())
+                format!(
+                    "www.example.com:{}",
+                    expected_protocol.default_port().unwrap_or(80)
+                )
             );
         }
     }

rama-net/src/proto.rs
@@ -207,14 +207,13 @@ impl Protocol {
         }
     }
 
-    /// Return a port that can be used as default in case no port is defined.
-    ///
-    /// NOTE that this is not going to be valid for non-http ports.
-    pub fn default_port(&self) -> u16 {
+    /// Returns the default port for this [`Protocol`]
+    pub fn default_port(&self) -> Option<u16> {
         match &self.0 {
-            ProtocolKind::Https | ProtocolKind::Wss => 443,
-            ProtocolKind::Http | ProtocolKind::Ws => 80,
-            ProtocolKind::Socks5 | ProtocolKind::Socks5h | ProtocolKind::Custom(_) => 80, // \_(ツ)_/¯
+            ProtocolKind::Https | ProtocolKind::Wss => Some(443),
+            ProtocolKind::Http | ProtocolKind::Ws => Some(80),
+            ProtocolKind::Socks5 | ProtocolKind::Socks5h => Some(1080),
+            ProtocolKind::Custom(_) => None,
         }
     }
 

rama-ua/src/emulate/service.rs
@@ -791,7 +791,7 @@ fn compute_sec_fetch_site_value(
 
                     let default_port = uri
                         .port_u16()
-                        .or_else(|| referer_protocol.as_ref().map(|p| p.default_port()));
+                        .or_else(|| referer_protocol.as_ref().and_then(|p| p.default_port()));
 
                     let maybe_authority = uri
                         .host()
@@ -903,7 +903,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::EXAMPLE_NAME,
-                    Protocol::HTTP.default_port(),
+                    Protocol::HTTP.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTP,
                 requested_client_hints: None,
@@ -920,7 +920,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::EXAMPLE_NAME,
-                    Protocol::HTTP.default_port(),
+                    Protocol::HTTP.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTP,
                 requested_client_hints: None,
@@ -937,7 +937,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::EXAMPLE_NAME,
-                    Protocol::HTTP.default_port(),
+                    Protocol::HTTP.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTP,
                 requested_client_hints: None,
@@ -951,7 +951,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::EXAMPLE_NAME,
-                    Protocol::HTTP.default_port(),
+                    Protocol::HTTP.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTP,
                 requested_client_hints: None,
@@ -965,7 +965,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::EXAMPLE_NAME,
-                    Protocol::HTTP.default_port(),
+                    Protocol::HTTP.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTP,
                 requested_client_hints: None,
@@ -990,7 +990,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::EXAMPLE_NAME,
-                    Protocol::HTTP.default_port(),
+                    Protocol::HTTP.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTP,
                 requested_client_hints: None,
@@ -1015,7 +1015,7 @@ mod tests {
                 preserve_ua_header: true,
                 request_authority: Authority::new(
                     Host::EXAMPLE_NAME,
-                    Protocol::HTTP.default_port(),
+                    Protocol::HTTP.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTP,
                 requested_client_hints: None,
@@ -1041,7 +1041,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::EXAMPLE_NAME,
-                    Protocol::HTTP.default_port(),
+                    Protocol::HTTP.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTP,
                 requested_client_hints: None,
@@ -1074,7 +1074,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::from_str("ramaproxy.org").unwrap(),
-                    Protocol::HTTPS.default_port(),
+                    Protocol::HTTPS.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTPS,
                 requested_client_hints: None,
@@ -1111,7 +1111,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::EXAMPLE_NAME,
-                    Protocol::HTTPS.default_port(),
+                    Protocol::HTTPS.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTPS,
                 requested_client_hints: None,
@@ -1150,7 +1150,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::from_str("ramaproxy.org").unwrap(),
-                    Protocol::HTTPS.default_port(),
+                    Protocol::HTTPS.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTPS,
                 requested_client_hints: None,
@@ -1207,7 +1207,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::from_str("www.google.com").unwrap(),
-                    Protocol::HTTP.default_port(),
+                    Protocol::HTTP.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTP,
                 requested_client_hints: None,
@@ -1277,7 +1277,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::from_str("www.google.com").unwrap(),
-                    Protocol::HTTPS.default_port(),
+                    Protocol::HTTPS.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTPS,
                 requested_client_hints: None,
@@ -1327,7 +1327,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::from_str("www.google.com").unwrap(),
-                    Protocol::HTTPS.default_port(),
+                    Protocol::HTTPS.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTPS,
                 requested_client_hints: None,
@@ -1400,7 +1400,7 @@ mod tests {
                 preserve_ua_header: false,
                 request_authority: Authority::new(
                     Host::from_str("www.google.com").unwrap(),
-                    Protocol::HTTPS.default_port(),
+                    Protocol::HTTPS.default_port().unwrap(),
                 ),
                 protocol: Protocol::HTTPS,
                 requested_client_hints: Some(vec![

commit ec6a862cab9e0df7154b9acc4f862d80bad96008

fix broken test related to socks5 port
issue introduced in https://github.com/plabayo/rama/pull/506,
and shipped as part of 0.2.0-alpha.9

luckily it's not a actual lib code bug or cli bug,
but just a test that had to be updated, so no harm done

rama-net/src/address/proxy.rs
@@ -281,20 +281,24 @@ mod tests {
                         let credential = it.next().unwrap();
                         let host = it.next().unwrap();
                         s = match scheme {
-                            Some(scheme) => format!("{scheme}://{credential}@[{host}]:80"),
+                            Some(scheme) => format!("{scheme}://{credential}@[{host}]:1080"),
                             None => format!("{credential}@[{host}]:80"),
                         };
                     } else {
                         s = match scheme {
-                            Some(scheme) => format!("{scheme}://[{host}]:80"),
+                            Some(scheme) => format!("{scheme}://[{host}]:1080"),
                             None => format!("[{host}]:80"),
                         };
                     }
                 } else {
-                    s = format!("{s}:80");
+                    s = if s.contains("://") {
+                        format!("{s}:1080")
+                    } else {
+                        format!("{s}:80")
+                    };
                 }
             }
-            assert_eq!(s, out);
+            assert_eq!(s, out, "addr: {addr}");
         }
     }
 }