11f51af56014ffb0772f2299ce693609cdbf0977
Glen De Cauwsemaecker <contact@glendc.com>
2025-05-23 19:08:51 +0200

    add tls sni router example + minor improvements

---
+++ tests/integration/examples/example_tests/tls_sni_router.rs
@@ -0,0 +1,43 @@
+use std::net::IpAddr;
+
+use super::utils;
+use rama::{Context, http::BodyExtractExt};
+use rama_dns::{DnsOverwrite, InMemoryDns};
+use rama_net::address::Domain;
+
+#[tokio::test]
+#[ignore]
+async fn test_tls_sni_router() {
+    utils::init_tracing();
+
+    let runner = utils::ExampleRunner::interactive("tls_sni_router", Some("boring"));
+
+    let mut ctx = Context::default();
+    let mut mem_dns = InMemoryDns::new();
+    mem_dns.insert_address(
+        Domain::from_static("foo.local"),
+        IpAddr::V4([127, 0, 0, 1].into()),
+    );
+    mem_dns.insert_address(
+        Domain::from_static("bar.local"),
+        IpAddr::V4([127, 0, 0, 1].into()),
+    );
+    ctx.insert(DnsOverwrite::from(mem_dns));
+
+    for (uri, expected_response) in [
+        ("https://127.0.0.1:63804", "foo"),
+        ("https://127.0.0.1:63805", "bar"),
+        ("https://foo.local:62026", "foo"),
+        ("https://bar.local:62026", "bar"),
+    ] {
+        let response = runner
+            .get(uri)
+            .send(ctx.clone())
+            .await
+            .unwrap()
+            .try_into_string()
+            .await
+            .unwrap();
+        assert_eq!(expected_response, response);
+    }
+}
