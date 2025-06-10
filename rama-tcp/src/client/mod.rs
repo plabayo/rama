f22e91e66e8b236ac30c6dd327edbfd2841ce5ae
Shabbir Hasan <68828793+ShabbirHasan1@users.noreply.github.com>
2025-06-10 18:42:12 +0530

    Add TCP stream connection pool functionality

--- rama-tcp/src/client/mod.rs
+++ rama-tcp/src/client/mod.rs
@@ -7,6 +7,17 @@ mod connect;
 #[doc(inline)]
 pub use connect::{TcpStreamConnector, default_tcp_connect, tcp_connect};
 
+mod pool;
+#[doc(inline)]
+pub use pool::{
+    PoolMode, TcpStreamConnectorPool,
+    ipcidr_connector::IpCidrConnector,
+    utils::{
+        IpCidrConExt, IpCidrConExtUsernameLabelParser, ipv4_from_extension, ipv4_with_range,
+        ipv6_from_extension, ipv6_with_range, rand_ipv4, rand_ipv6,
+    },
+};
+
 #[cfg(feature = "http")]
 mod request;
 #[cfg(feature = "http")]
