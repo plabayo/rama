d6c171d78ce408582da8ffe84942989d81b7af9b
Aydan Pirani <aydanpirani@gmail.com>
2025-07-18 01:32:23 -0700

    more changes

--- rama-http-types/src/curl/mod.rs
+++ rama-http-types/src/curl/mod.rs
@@ -1,27 +1,41 @@
-use http::{HeaderMap, HeaderValue, Method, Request, Uri, Version};
+use crate::{HeaderMap, HeaderValue, Method, Request, Uri, Version, Body};
+use http::{Extensions};
 use rama_error::OpaqueError;
+// use rama_net::address::ProxyAddress;
 
-// TODO: make sealed(private) trait, and add sealed(private) impl for Request<T>
-pub trait IntoCurlHeadersCommand {
-    fn to_curl_command(&self) -> Result<String, OpaqueError>;
+pub fn request_headers_to_curl_command<B>(request: Request<B>) -> Result<String, OpaqueError> {
+    let method = request.method();
+    let uri = request.uri();
+    let version = request.version();
+    let headers = request.headers();
+    let extensions = request.extensions();
+    
+    generate_curl_command(method, uri, version, headers, extensions)
 }
 
-impl<T> IntoCurlHeadersCommand for Request<T> {
-    fn to_curl_command(&self) -> Result<String, OpaqueError> {
-        let method = self.method();
-        let uri = self.uri();
-        let version = self.version();
-        let headers = self.headers();
+pub async fn request_to_curl_command<B: Into<Body>>(request: Request<B>) -> Result<String, OpaqueError> {
+    let method: &Method = request.method();
+    let uri = request.uri();
+    let version = request.version();
+    let headers = request.headers();
+    let extensions = request.extensions();
+    
+    let mut cmd = generate_curl_command(method, uri, version, headers, extensions)?;
+    
 
-        generate_curl_command(method, uri, version, headers)
-    }
+    let b: B = request.body().;
+    println!("{:?}", b);
+    // grab the bytes, then turn into UTF-8 (lossy avoids errors)
+    // let bytes = b.as_ref();
+    // let s = String::from_utf8_lossy(bytes);
+    // cmd.push_str(&format!(" --data '{}'", s));
+
+
+    Ok(cmd)
 }
 
-pub fn request_headers_to_curl_command(request: impl IntoCurlHeadersCommand) -> Result<String, OpaqueError> {
-    request.to_curl_command()
-}
 
-fn generate_curl_command(method: &Method, uri: &Uri, version: Version, headers: &HeaderMap<HeaderValue>) -> Result<String, OpaqueError>{
+fn generate_curl_command(method: &Method, uri: &Uri, version: Version, headers: &HeaderMap<HeaderValue>, extensions: &Extensions) -> Result<String, OpaqueError>{
     let mut cmd = format!("curl -X {} '{}'", method, uri);
 
     match version {
@@ -30,7 +44,7 @@ fn generate_curl_command(method: &Method, uri: &Uri, version: Version, headers: 
         Version::HTTP_11 => cmd.push_str(" --http1.1"),
         Version::HTTP_2 => cmd.push_str(" --http2"),
         Version::HTTP_3 => cmd.push_str(" --http3"),
-        _ => panic!("Unexpected HTTP version!"),
+        _ => {return Err(OpaqueError::from_display("Unexpected HTTP version!"))},
     }
 
     for (name, value) in headers.iter() {
@@ -38,8 +52,12 @@ fn generate_curl_command(method: &Method, uri: &Uri, version: Version, headers: 
             Ok(s) => s,
             Err(_) => continue, // skip invalid UTF-8 headers
         };
-        cmd.push_str(&format!(r#" -H "{}: {}""#, name.as_str(), val));
+        cmd.push_str(&format!(r#" -H '{}: {}'"#, name.as_str(), val));
     }
+
+    // if let Some(proxy) = extensions.get::<ProxyAddress>() {
+    //     cmd.push_str(&format!(" --proxy '{}'", proxy.as_str()));
+    // }
 
     Ok(cmd)
 }
