0c74e1515d16930c33e94789f8c73072b2618e9f
Dominic Lindsay <dominic.lindsay@cryptio.co>
2025-07-20 23:55:21 +0100

    Skeleton

--- tests-turmoil/test-client-server/src/main.rs
+++ tests-turmoil/test-client-server/src/main.rs
@@ -1,3 +1,13 @@
+use turmoil::Builder;
+
 fn main() {
-    println!("Hello, world!");
+    // NOTE:
+    // To make your tests deterministic, you can use your own seeded `rng` provider when building
+    // the simulation through `build_with_rng`.
+
+    let mut sim = Builder::new().build();
+
+    sim.host("server", || async { Ok(()) });
+
+    sim.client("client", async { Ok(()) });
 }
