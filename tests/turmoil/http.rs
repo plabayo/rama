0d4a824bd0c95069225db3f4c8f92afc5f45270a
Dominic Lindsay <dominic.lindsay@cryptio.co>
2025-07-25 21:56:19 +0100

    fmt

--- tests/turmoil/http.rs
+++ tests/turmoil/http.rs
@@ -1,13 +1,13 @@
 use rama::{
+    Context, Layer, Service,
     http::{
-        client::EasyHttpWebClient, layer::trace::TraceLayer, server::HttpServer,
-        service::web::WebService, Body, BodyExtractExt, Request,
+        Body, BodyExtractExt, Request, client::EasyHttpWebClient, layer::trace::TraceLayer,
+        server::HttpServer, service::web::WebService,
     },
     net::address::SocketAddress,
-    Context, Layer, Service,
 };
 use tracing_subscriber::{
-    filter::LevelFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter,
+    EnvFilter, filter::LevelFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt,
 };
 use turmoil::Builder;
 
