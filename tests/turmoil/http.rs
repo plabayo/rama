6286366bc080078a09ee0b6ce635ef1e59263792
Dominic Lindsay <dominic.lindsay@cryptio.co>
2025-08-03 23:45:50 +0100

    fmt

--- tests/turmoil/http.rs
+++ tests/turmoil/http.rs
@@ -2,15 +2,15 @@ use std::time::Duration;
 
 use http::Version;
 use rama::{
+    Context, Layer, Service,
     http::{
-        layer::trace::TraceLayer, server::HttpServer, service::web::WebService, Body,
-        BodyExtractExt, Request,
+        Body, BodyExtractExt, Request, layer::trace::TraceLayer, server::HttpServer,
+        service::web::WebService,
     },
     net::address::SocketAddress,
-    Context, Layer, Service,
 };
 use rama_http_backend::client::EasyHttpWebClientBuilder;
-use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 use turmoil::{Builder, ToSocketAddrs};
 
 use crate::types::TurmoilTcpConnector;
