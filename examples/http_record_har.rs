ea40d1666f6542d5a1133e3869118c76cd4ba936
MeerkatDev <lcampobasso@gmail.com>
2025-08-11 19:11:55 +0200

    example modded but still failing

--- examples/http_record_har.rs
+++ examples/http_record_har.rs
@@ -6,7 +6,15 @@
 //! cargo run --example http_record_har --features=http-full
 //! ```
 use rama::{
-    Layer, graceful::Shutdown, http::server::HttpServer, rt::Executor, tcp::server::TcpListener,
+    Context,
+    Layer, 
+    graceful::Shutdown, 
+    http::Request,
+    http::layer::har::layer::HARExportLayer,
+    http::service::web::response::Html,
+    service::service_fn,
+    layer::TimeoutLayer,
+    http::server::HttpServer, rt::Executor, tcp::server::TcpListener,
 };
 
 use std::time::Duration;
@@ -18,9 +26,35 @@ async fn main() {
     graceful.spawn_task_fn(async |guard| {
         let exec = Executor::graceful(guard.clone());
 
-        let http_service = (HARExportLayer::new()).into_layer();
+        let http_service = (HARExportLayer::default())
+            .into_layer(service_fn(async |ctx: Context<()>, req: Request| {
+                Ok(Html(format!(
+                    r##"
+                    <html>
+                        <head>
+                            <title>Rama — Http Service Hello</title>
+                        </head>
+                        <body>
+                            <h1>Hello</h1>
+                        </body>
+                    </html>
+                    "##,
+                )))
+        }));
 
         let tcp_http_service = HttpServer::auto(exec).service(http_service);
+
+        TcpListener::bind("127.0.0.1:62010")
+            .await
+            .expect("bind TCP Listener")
+            .serve_graceful(
+                guard,
+                (
+                    TimeoutLayer::new(Duration::from_secs(8))
+                )
+                    .into_layer(tcp_http_service),
+            )
+            .await;
     });
 
     graceful
