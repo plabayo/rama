24d03b5888e37fe1ea8c85367db028816a8fbe82
Glen De Cauwsemaecker <contact@glendc.com>
2025-08-17 22:17:22 +0200

    add couple of WS compression tests based on examples from RFC 7692

--- rama-ws/src/protocol/tests/write.rs
+++ rama-ws/src/protocol/tests/write.rs
@@ -1,8 +1,10 @@
 use crate::{
     Message,
-    protocol::{Role, WebSocket, WebSocketConfig, error::ProtocolError},
+    protocol::{PerMessageDeflateConfig, Role, WebSocket, WebSocketConfig, error::ProtocolError},
 };
+use rama_core::telemetry::tracing;
 use std::io::{self, Cursor, Read, Write};
+use tracing_test::traced_test;
 
 pin_project_lite::pin_project! {
     struct WriteMoc<Stream> {
@@ -62,6 +64,44 @@ fn receive_messages() {
         socket.read().unwrap(),
         Message::Binary(vec![0x01, 0x02, 0x03].into())
     );
+}
+
+#[test]
+#[traced_test]
+fn receive_compressed_hello_text_msg_rfc7692_example_compression_1() {
+    let incoming = Cursor::new(vec![0xc1, 0x07, 0xf2, 0x48, 0xcd, 0xc9, 0xc9, 0x07, 0x00]);
+
+    let mut socket = WebSocket::from_raw_socket(
+        WriteMoc::new(incoming),
+        Role::Client,
+        Some(WebSocketConfig {
+            per_message_deflate: Some(PerMessageDeflateConfig::default()),
+            ..Default::default()
+        }),
+    );
+
+    assert_eq!(socket.read().unwrap(), Message::Text("Hello".into()));
+}
+
+#[test]
+#[traced_test]
+fn receive_compressed_hello_text_msg_rfc7692_example_compression_2() {
+    #[rustfmt::skip]
+    let incoming = Cursor::new(vec![
+        0x41, 0x03, 0xf2, 0x48, 0xcd,       // fragment #1
+        0x80, 0x04, 0xc9, 0xc9, 0x07, 0x00, // fragment #2
+    ]);
+
+    let mut socket = WebSocket::from_raw_socket(
+        WriteMoc::new(incoming),
+        Role::Client,
+        Some(WebSocketConfig {
+            per_message_deflate: Some(PerMessageDeflateConfig::default()),
+            ..Default::default()
+        }),
+    );
+
+    assert_eq!(socket.read().unwrap(), Message::Text("Hello".into()));
 }
 
 #[test]
