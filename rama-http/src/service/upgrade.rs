4688a73c8ef8fd1a8774a98cdd74015b305b84e8
Kenny Lau <72945813+lauk20@users.noreply.github.com>
2025-08-25 17:29:58 -0400

    add http-to-https 'upgrade' Redirect (SS) service (#678)
    Closes #658 
    add http-to-https 'upgrade' Redirect (SS) service.

---
+++ rama-http/src/service/upgrade.rs
@@ -0,0 +1,76 @@
+//! Service that redirects all HTTP requests to HTTPS
+
+use crate::Request;
+use crate::StatusCode;
+use crate::service::web::response::IntoResponse;
+use crate::{Response, header};
+use rama_core::{Context, Service, telemetry::tracing};
+use rama_net::{Protocol, http::RequestContext};
+use rama_utils::macros::generate_set_and_with;
+use std::convert::Infallible;
+
+/// Service that redirects all HTTP requests to HTTPS
+#[derive(Debug, Clone)]
+pub struct Upgrade {
+    status_code: StatusCode,
+}
+
+impl Upgrade {
+    generate_set_and_with! {
+        /// Set status_code in the Upgrade struct
+        pub fn status_code(mut self, status_code: StatusCode) -> Self {
+            self.status_code = status_code;
+            self
+        }
+    }
+}
+
+impl<State, Body> Service<State, Request<Body>> for Upgrade
+where
+    State: Clone + Send + Sync + 'static,
+    Body: Send + 'static,
+{
+    type Response = Response;
+    type Error = Infallible;
+
+    async fn serve(
+        &self,
+        mut ctx: Context<State>,
+        req: Request<Body>,
+    ) -> Result<Self::Response, Self::Error> {
+        let req_ctx: &mut RequestContext = match ctx
+            .get_or_try_insert_with_ctx(|ctx| (ctx, &req).try_into())
+        {
+            Ok(req_ctx) => req_ctx,
+            Err(err) => {
+                tracing::error!("failed to get RequestContext for insecure incoming req: {err}");
+                return Ok(StatusCode::BAD_GATEWAY.into_response());
+            }
+        };
+        let host = &req_ctx.authority.host();
+        let upgraded_protocol = match req_ctx.protocol {
+            Protocol::HTTP => Protocol::HTTPS.as_str(),
+            Protocol::WS => Protocol::WSS.as_str(),
+            _ => {
+                tracing::error!("unexpected protocol: {}", req_ctx.protocol);
+                return Ok(StatusCode::BAD_GATEWAY.into_response());
+            }
+        };
+        let paq = req
+            .uri()
+            .path_and_query()
+            .map(|paq| paq.as_str())
+            .unwrap_or("/");
+        let loc = format!("{upgraded_protocol}://{host}{paq}");
+
+        Ok(([(header::LOCATION, loc)], self.status_code).into_response())
+    }
+}
+
+impl Default for Upgrade {
+    fn default() -> Self {
+        Self {
+            status_code: StatusCode::PERMANENT_REDIRECT,
+        }
+    }
+}
