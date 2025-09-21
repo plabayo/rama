ef3ebc69d4fbb447e8f549f9e0e5177aafd8610d
nagxsan <sanchetng@gmail.com>
2025-09-21 18:19:27 +0530

    feat: add parse_pac_file fn and iterative calls for proxy list options

--- rama-pac/src/connector/mod.rs
+++ rama-pac/src/connector/mod.rs
@@ -1,11 +1,8 @@
+use crate::pac_parser::ProxyDirective;
 use crate::{pac_fetcher::fetch_pac, pac_parser::parse_pac_file};
 use rama_core::{Context, Service, error::BoxError};
 use rama_http::{Uri, service::client::HttpClientExt};
-use rama_net::{
-    client::{ConnectorService, EstablishedClientConnection},
-    stream::{Socket, Stream},
-};
-use std::fmt::Debug;
+use rama_http_types::Request;
 
 pub struct PACConnector<S, W> {
     pub service: S,
@@ -26,30 +23,75 @@ where
     }
 }
 
-impl<S, W, Request> Service<Request> for PACConnector<S, W>
+impl<S, W, Body> Service<Request<Body>> for PACConnector<S, W>
 where
-    S: ConnectorService<Request, Connection: Stream + Socket + Unpin, Error: Into<BoxError>>
-        + Send
-        + Sync
-        + 'static,
+    S: Service<Request<Body>, Error: Into<BoxError> + Send + Sync + 'static>,
     W: HttpClientExt + Send + Sync + 'static,
-    Request: Send + 'static,
-    <W as HttpClientExt>::ExecuteResponse: Into<String>,
-    <W as HttpClientExt>::ExecuteError: Debug
+    Body: Clone + Send + Sync + 'static,
+    W::ExecuteError: std::error::Error + Send + Sync + 'static,
+    W::ExecuteResponse: Into<String>,
 {
-    type Response = EstablishedClientConnection<S::Connection, Request>;
+    type Response = S::Response;
     type Error = BoxError;
 
-    async fn serve(&self, ctx: Context, req: Request) -> Result<Self::Response, Self::Error> {
-        // first get the pac file
-        let pac_file = fetch_pac(&self.web_client, self.pac_uri.clone())
+    async fn serve(
+        &self,
+        ctx: Context,
+        req: Request<Body>,
+    ) -> Result<Self::Response, Self::Error> {
+        let pac_file = fetch_pac(&self.web_client, &self.pac_uri)
             .await
             .unwrap();
 
-        // parse and get back result for the uri form the pac file
-        let proxy_options = parse_pac_file(&pac_file);
+        let request_url = req.uri();
+        let proxy_options = parse_pac_file(request_url, &pac_file).unwrap();
 
-        // based on the result, either connect to the uri or return error
-        // Ok(self.service.connect(ctx, req).await.unwrap())
+        let req_template = req;
+        let mut last_err: Option<BoxError> = None;
+
+        for opt in proxy_options {
+            let mut attempt = req_template.clone();
+            match opt {
+                ProxyDirective::Direct => {}
+                ProxyDirective::Proxy(hp) | ProxyDirective::Http(hp) => {
+                    let proxy_uri: Uri = format!("http://{hp}")
+                        .parse()
+                        .map_err(|e| format!("bad PROXY uri {hp}: {e}"))?;
+                    *attempt.uri_mut() = proxy_uri;
+                }
+                ProxyDirective::Https(hp) => {
+                    let proxy_uri: Uri = format!("https://{hp}")
+                        .parse()
+                        .map_err(|e| format!("bad HTTPS proxy uri {hp}: {e}"))?;
+                    *attempt.uri_mut() = proxy_uri;
+                }
+                ProxyDirective::Socks(hp) => {
+                    let proxy_uri: Uri = format!("socks://{hp}")
+                        .parse()
+                        .map_err(|e| format!("bad socks uri {hp}: {e}"))?;
+                    *attempt.uri_mut() = proxy_uri;
+                }
+                ProxyDirective::Socks4(hp) => {
+                    let proxy_uri: Uri = format!("socks4://{hp}")
+                        .parse()
+                        .map_err(|e| format!("bad socks4 uri {hp}: {e}"))?;
+                    *attempt.uri_mut() = proxy_uri;
+                }
+                ProxyDirective::Socks5(hp) => {
+                    let proxy_uri: Uri = format!("socks5://{hp}")
+                        .parse()
+                        .map_err(|e| format!("bad socks5 uri {hp}: {e}"))?;
+                    *attempt.uri_mut() = proxy_uri;
+                }
+            }
+            match self.service.serve(ctx.clone(), attempt).await {
+                Ok(resp) => return Ok(resp),
+                Err(e) => {
+                    last_err = Some(e.into());
+                }
+            }
+        }
+
+        Err(last_err.unwrap_or_else(|| "no PAC options succeeded".into()))
     }
 }
--- rama-pac/src/pac_fetcher/mod.rs
+++ rama-pac/src/pac_fetcher/mod.rs
@@ -1,23 +1,21 @@
-use std::fmt::Debug;
-
 use rama_core::{Context, error::BoxError};
-use rama_http::{Body, Uri, dep::http::Request, service::client::HttpClientExt};
+use rama_http::{Body, Request, Uri, service::client::HttpClientExt};
 
-pub async fn fetch_pac<W>(web_client: &W, pac_uri: Uri) -> Result<String, BoxError>
+pub async fn fetch_pac<W>(web_client: &W, pac_uri: &Uri) -> Result<String, BoxError>
 where
-    W: HttpClientExt + Send + Sync + 'static,
-    <W as HttpClientExt>::ExecuteResponse: Into<String>,
-    <W as HttpClientExt>::ExecuteError: Debug,
+    W: HttpClientExt,
+    W::ExecuteError: std::error::Error + Send + Sync + 'static,
+    W::ExecuteResponse: Into<String>,
 {
     let request = Request::builder()
         .method("GET")
         .uri(pac_uri)
         .body(Body::empty())
-        .unwrap();
-    let response = web_client
-        .execute(Context::default(), request)
-        .await
-        .unwrap();
-    let pac_file = response.try_into().unwrap();
+        .map_err(Into::<BoxError>::into)?;
+
+    let response = web_client.execute(Context::default(), request).await?;
+
+    let pac_file: String = response.into();
+
     Ok(pac_file)
 }
