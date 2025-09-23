e6b04eb4a644d06ed63b90651ca5e3565d96e21b
brecht stamper <stamper.brecht@gmail.com>
2025-09-23 17:10:37 +0200

    Extractor for extensions

---
+++ rama-http/src/service/web/endpoint/extract/extensions.rs
@@ -0,0 +1,15 @@
+use super::FromRequestContextRefPair;
+use crate::request::Parts;
+use rama_core::{Context, context::Extensions};
+use std::convert::Infallible;
+
+impl FromRequestContextRefPair for Extensions {
+    type Rejection = Infallible;
+
+    async fn from_request_context_ref_pair(
+        _ctx: &Context,
+        parts: &Parts,
+    ) -> Result<Self, Self::Rejection> {
+        Ok(parts.extensions.clone())
+    }
+}
