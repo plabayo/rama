3a4573c101878a1962cb93c60551df414170c78e
brecht stamper <stamper.brecht@gmail.com>
2025-09-23 11:06:41 +0200

    rama-unix migrate to extensions and ext traits

---
+++ rama-unix/src/unix/stream.rs
@@ -0,0 +1,96 @@
+use std::{
+    io,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use pin_project_lite::pin_project;
+use rama_core::{
+    context::Extensions,
+    extensions::{ExtensionsMut, ExtensionsRef},
+};
+use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+pub use tokio::net::UnixStream as TokioUnixStream;
+
+pin_project! {
+    #[derive(Debug)]
+    /// A stream which can be either a secure or a plain stream.
+    pub struct UnixStream {
+        #[pin]
+        pub stream: TokioUnixStream,
+        pub extensions: Extensions
+    }
+}
+
+impl UnixStream {
+    pub fn new(stream: TokioUnixStream) -> Self {
+        Self {
+            stream,
+            extensions: Extensions::new(),
+        }
+    }
+}
+
+impl From<TokioUnixStream> for UnixStream {
+    fn from(value: TokioUnixStream) -> Self {
+        Self::new(value)
+    }
+}
+
+impl From<UnixStream> for TokioUnixStream {
+    fn from(value: UnixStream) -> Self {
+        value.stream
+    }
+}
+
+impl ExtensionsRef for UnixStream {
+    fn extensions(&self) -> &Extensions {
+        &self.extensions
+    }
+}
+
+impl ExtensionsMut for UnixStream {
+    fn extensions_mut(&mut self) -> &mut Extensions {
+        &mut self.extensions
+    }
+}
+
+impl AsyncRead for UnixStream {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        self.project().stream.poll_read(cx, buf)
+    }
+}
+
+impl AsyncWrite for UnixStream {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        self.project().stream.poll_write(cx, buf)
+    }
+
+    fn poll_write_vectored(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        bufs: &[io::IoSlice<'_>],
+    ) -> Poll<io::Result<usize>> {
+        self.project().stream.poll_write_vectored(cx, bufs)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.project().stream.poll_flush(cx)
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.project().stream.poll_shutdown(cx)
+    }
+
+    fn is_write_vectored(&self) -> bool {
+        self.stream.is_write_vectored()
+    }
+}
