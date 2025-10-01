44201912aa6680eebe07a6db45dba5a152b1d1b0
Glen De Cauwsemaecker <contact@glendc.com>
2025-10-02 00:58:12 +0200

    clippy fix ip

--- rama-cli/src/cmd/ip/mod.rs
+++ rama-cli/src/cmd/ip/mod.rs
@@ -100,7 +100,7 @@ pub async fn run(cfg: CliCommandIp) -> Result<(), BoxError> {
                 .build()
                 .expect("build ip TCP service"),
         ),
-        (false, true) | (false, false) => Either3::C(
+        (false, true | false) => Either3::C(
             IpServiceBuilder::http()
                 .with_concurrent(cfg.concurrent)
                 .with_timeout(Duration::from_secs(cfg.timeout))
