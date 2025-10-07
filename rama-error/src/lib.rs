53136718a352d25f2bb97bb3c2da1b63ed23671b
Glen De Cauwsemaecker <contact@glendc.com>
2025-10-07 19:45:06 +0200

    docs: Remove doc_auto_cfg config

--- rama-error/src/lib.rs
+++ rama-error/src/lib.rs
@@ -179,7 +179,7 @@
     html_favicon_url = "https://raw.githubusercontent.com/plabayo/rama/main/docs/img/old_logo.png"
 )]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/plabayo/rama/main/docs/img/old_logo.png")]
-#![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(test, allow(clippy::float_cmp))]
 #![cfg_attr(not(test), warn(clippy::print_stdout, clippy::dbg_macro))]
 
