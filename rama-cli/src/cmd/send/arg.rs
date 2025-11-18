707a16b9af1fd82544dd0bd1ad815c40fd308151
glendc <glen@plabayo.tech>
2025-11-18 18:56:56 +0100

    add tests for ResolveArg parsing (rama-cli >> send --resolve)

--- rama-cli/src/cmd/send/arg.rs
+++ rama-cli/src/cmd/send/arg.rs
@@ -37,6 +37,7 @@ impl fmt::Display for TlsVersion {
 }
 
 #[derive(Debug, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
 pub(super) struct ResolveArg {
     pub(super) host: Option<Domain>,
     pub(super) port: Option<u16>,
@@ -55,7 +56,17 @@ impl FromStr for ResolveArg {
 
         let (raw_host, s) = s.split_once(':').context("split host part")?;
         if !raw_host.is_empty() && raw_host != "*" {
-            arg.host = Some(raw_host.parse().context("parse host as domain")?);
+            let host: Domain = raw_host.parse().context("parse host as domain")?;
+            if !host
+                .suffix()
+                .map(|suffix| suffix.chars().all(|c| c.is_alphabetic() || c == '.'))
+                .unwrap_or_default()
+            {
+                return Err(OpaqueError::from_display(format!(
+                    "invalid domain (suffix not found or invalid): {host}"
+                )));
+            }
+            arg.host = Some(host);
         }
 
         let (raw_port, s) = s.split_once(':').context("split port part")?;
@@ -75,5 +86,137 @@ impl FromStr for ResolveArg {
         }
 
         Ok(arg)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rama::net::address::SocketAddress;
+
+    use super::*;
+
+    #[test]
+    fn test_resolve_arg_from_str() {
+        for (input, expected_value) in [
+            ("", None),
+            (
+                "::127.0.0.1",
+                Some(ResolveArg {
+                    host: None,
+                    port: None,
+                    addresses: vec![SocketAddress::local_ipv4(0).ip_addr],
+                }),
+            ),
+            (
+                "example.com::127.0.0.1",
+                Some(ResolveArg {
+                    host: Some(Domain::example()),
+                    port: None,
+                    addresses: vec![SocketAddress::local_ipv4(0).ip_addr],
+                }),
+            ),
+            (
+                "example.com:42:127.0.0.1",
+                Some(ResolveArg {
+                    host: Some(Domain::example()),
+                    port: Some(42),
+                    addresses: vec![SocketAddress::local_ipv4(0).ip_addr],
+                }),
+            ),
+            (
+                "localhost:42:127.0.0.1",
+                Some(ResolveArg {
+                    host: Some(Domain::tld_localhost()),
+                    port: Some(42),
+                    addresses: vec![SocketAddress::local_ipv4(0).ip_addr],
+                }),
+            ),
+            (
+                "venndb.internal:42:127.0.0.1",
+                Some(ResolveArg {
+                    host: Some(Domain::from_static("venndb.internal")),
+                    port: Some(42),
+                    addresses: vec![SocketAddress::local_ipv4(0).ip_addr],
+                }),
+            ),
+            (
+                "www.example.com:8080:::1",
+                Some(ResolveArg {
+                    host: Some(Domain::from_static("www.example.com")),
+                    port: Some(8080),
+                    addresses: vec![SocketAddress::local_ipv6(0).ip_addr],
+                }),
+            ),
+            (
+                "www.example.co.uk:8080:::1",
+                Some(ResolveArg {
+                    host: Some(Domain::from_static("www.example.co.uk")),
+                    port: Some(8080),
+                    addresses: vec![SocketAddress::local_ipv6(0).ip_addr],
+                }),
+            ),
+            (
+                "*:8080:::1",
+                Some(ResolveArg {
+                    host: None,
+                    port: Some(8080),
+                    addresses: vec![SocketAddress::local_ipv6(0).ip_addr],
+                }),
+            ),
+            (
+                "www.example.com:*:::1",
+                Some(ResolveArg {
+                    host: Some(Domain::from_static("www.example.com")),
+                    port: None,
+                    addresses: vec![SocketAddress::local_ipv6(0).ip_addr],
+                }),
+            ),
+            (
+                "*:*:::1",
+                Some(ResolveArg {
+                    host: None,
+                    port: None,
+                    addresses: vec![SocketAddress::local_ipv6(0).ip_addr],
+                }),
+            ),
+            (
+                "*::::1",
+                Some(ResolveArg {
+                    host: None,
+                    port: None,
+                    addresses: vec![SocketAddress::local_ipv6(0).ip_addr],
+                }),
+            ),
+            (
+                ":*:::1",
+                Some(ResolveArg {
+                    host: None,
+                    port: None,
+                    addresses: vec![SocketAddress::local_ipv6(0).ip_addr],
+                }),
+            ),
+            (
+                "www.example.com:8080:::1,127.0.0.1,127.0.0.2",
+                Some(ResolveArg {
+                    host: Some(Domain::from_static("www.example.com")),
+                    port: Some(8080),
+                    addresses: vec![
+                        SocketAddress::local_ipv6(0).ip_addr,
+                        SocketAddress::local_ipv4(0).ip_addr,
+                        [127, 0, 0, 2].into(),
+                    ],
+                }),
+            ),
+            ("127.0.0.1:42:127.0.0.1", None),
+            ("::1:42:127.0.0.1", None),
+            ("42:example.com:127.0.0.1", None),
+            ("127.0.0.1:42:example.com", None),
+        ] {
+            let result: Result<ResolveArg, _> = input.parse();
+            match expected_value {
+                Some(value) => assert_eq!(Some(value), result.ok(), "input: '{input}'"),
+                None => assert!(result.is_err(), "expected error for input: '{input}'"),
+            }
+        }
     }
 }
