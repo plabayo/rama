87c28b3e94f0bf6cf30aa9de41c2c063cb2dd9be
Mantra <mantrakusumgar@outlook.com>
2025-12-15 23:42:31 +0000

    add skeleton for e2e h1 client server interaction

---
+++ benches/e2e_h1_client_server.rs
@@ -0,0 +1,96 @@
+use rama::{
+    Layer,
+    Service,
+    telemetry::tracing::{
+        self,
+        level_filters::LevelFilter,
+        subscriber::{
+            EnvFilter,
+            fmt,
+            layer::SubscriberExt,
+            util::SubscriberInitExt
+        },
+    },
+    http::{
+        layer::{
+            trace::TraceLayer,
+            compression::CompressionLayer,
+            decompression::DecompressionLayer,
+        },
+        server::HttpServer,
+        client::EasyHttpWebClient,
+        Request,
+        Body,
+        service::web::WebService,
+    },
+    net::address::SocketAddress,
+};
+
+use tokio_test::block_on;
+
+const ADDRESS: SocketAddress = SocketAddress::local_ipv4(62004);
+
+#[global_allocator]
+static ALLOC: divan::AllocProfiler = divan::AllocProfiler::system();
+
+fn main() {
+    // Make Tokio available for the duration of the process:
+    let executor = tokio::runtime::Runtime::new().unwrap();
+    let _guard = executor.enter();
+
+    // Run registered benchmarks.
+    divan::main();
+}
+
+#[divan::bench]
+fn h1_client_server_small_payload(b: divan::Bencher) {
+    setup_tracing();
+    tokio::spawn(run_server());
+
+    let client = (
+        TraceLayer::new_for_http(),
+        DecompressionLayer::new(),
+    )
+        .into_layer(EasyHttpWebClient::default());
+
+    b.bench_local(|| block_on(get_small_payload(client.clone())));
+}
+
+fn setup_tracing() {
+    tracing::subscriber::registry()
+        .with(fmt::layer())
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+}
+
+async fn run_server() {
+    let http_service = (
+        CompressionLayer::new(),
+        TraceLayer::new_for_http(),
+    )
+        .into_layer(
+            WebService::default()
+                .with_get("/small-payload", "Super small payload")
+        );
+
+    HttpServer::http1()
+        .listen(ADDRESS, http_service)
+        .await
+        .unwrap();
+}
+
+async fn get_small_payload(client: impl Service<Request>) {
+    let req = Request::builder()
+        .uri(format!("http://{ADDRESS}/small-payload"))
+        .method("GET")
+        .body(Body::empty())
+        .unwrap();
+
+    let _ = client
+        .serve(req)
+        .await;
+}
