51376858384b2011f289df7b9f8fb6db2560f206
glendc <glen@plabayo.tech>
2026-01-04 22:07:03 +0100

    fix CI failures (first attempt)

--- scripts/extra-checks.sh
+++ scripts/extra-checks.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 exit_code=0
 
 # Make sure all examples are included in the rama book
-for example in $(cd $SCRIPT_DIR/.. && find examples -type f -name '*.rs' -not -name 'mod.rs'); do
+for example in $(cd $SCRIPT_DIR/.. && find examples -maxdepth 1 -type f -name '*.rs' -not -name 'mod.rs'); do
     echo "Checking $example..."
     if ! grep -qr "$example" docs/book; then
         echo "❌ Example $example, missing in rama book"
