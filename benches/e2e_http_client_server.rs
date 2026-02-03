d21e256df25151c6005dd2f30c0361a7291869e2
Glen De Cauwsemaecker <glen@plabayo.tech>
2026-02-03 14:54:40 +0100

    Issue 374/Benchmark for e2e h1 client server interaction (#766) (#816)
    Continuation of PR https://github.com/plabayo/rama/pull/766
    minor cleanups etc
    
    ## Run
    
    `cargo bench --features=http-full --bench e2e_http_client_server`
    
    Time Results
    
    ```
    HTTP/1 (over TCP)
    S S  122.5 µs  ████████
    S L  168.4 µs  ███████████
    L S  327.3 µs  ███████████████████████
    L L  577.1 µs  ████████████████████████████████████████
    
    HTTP/2 (over TCP)
    S S  170.2 µs  ███████████
    S L  314.5 µs  ██████████████████████
    L S  479.4 µs  ██████████████████████████████████
    L L  639.6 µs  ██████████████████████████████████████████████
    ```
    
    Allocations:
    
    ```
    HTTP/1  (over TCP)
    S S   105.5 KB ███
    S L   604.5 KB ███████████████
    L S   1.401 MB ████████████████████████████████████
    L L   1.957 MB ██████████████████████████████████████████████████
    
    HTTP/2 (over TCP)
    S S   139.6 KB ████
    S L   681.9 KB █████████████████
    L S   1.238 MB ████████████████████████████████
    L L   1.781 MB ██████████████████████████████████████████████
    ```
    
    Once we have more and more e2e protocol results I'll make something so
    we can attach these to each PR and track regressions as well (within
    marges).
    
    ---------
    
    Co-authored-by: M-Kusumgar <98405247+M-Kusumgar@users.noreply.github.com>

---
+++ benches/e2e_http_client_server.rs
@@ -0,0 +1,231 @@
+use std::{
+    convert::Infallible,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+};
+
+use rama::{
+    Layer, Service,
+    error::BoxError,
+    http::{
+        Body, HeaderName, HeaderValue, Request, Response, Version,
+        body::util::BodyExt,
+        client::EasyHttpWebClient,
+        layer::{
+            compression::CompressionLayer,
+            cors::CorsLayer,
+            decompression::DecompressionLayer,
+            map_response_body::MapResponseBodyLayer,
+            required_header::{AddRequiredRequestHeadersLayer, AddRequiredResponseHeadersLayer},
+            set_header::{SetRequestHeaderLayer, SetResponseHeaderLayer},
+            trace::TraceLayer,
+        },
+        server::HttpServer,
+        service::{
+            client::HttpClientExt as _,
+            web::{WebService, response::IntoResponse as _},
+        },
+    },
+    net::address::SocketAddress,
+    rt::Executor,
+    tcp::server::TcpListener,
+    telemetry::tracing,
+};
+
+use rand::prelude::*;
+
+pub mod e2e_utils;
+
+#[global_allocator]
+static ALLOC: divan::AllocProfiler = divan::AllocProfiler::system();
+
+fn main() {
+    let _appender_guard = e2e_utils::setup_tracing("e2e_http_client_server");
+    divan::main();
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Size {
+    Small,
+    Large,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum HttpVersion {
+    Http1,
+    Http2,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct TestParameters {
+    version: HttpVersion,
+    server: Size,
+    client: Size,
+}
+
+fn random_bytes_by_size(rng: &mut ThreadRng, size: Size) -> Vec<u8> {
+    match size {
+        Size::Small => {
+            let mut bytes = [0u8; 1_000];
+            rng.fill_bytes(&mut bytes);
+            bytes.to_vec()
+        }
+        Size::Large => {
+            let mut bytes = [0u8; 500_000];
+            rng.fill_bytes(&mut bytes);
+            bytes.to_vec()
+        }
+    }
+}
+
+fn get_endpoint_for_size(size: Size) -> &'static str {
+    match size {
+        Size::Small => "small",
+        Size::Large => "large",
+    }
+}
+
+fn spawn_http_server(params: TestParameters, body_content: Vec<u8>) -> SocketAddress {
+    let body_content: &[u8] = body_content.leak();
+    let handler = async move |req: Request| {
+        if let Err(err) = req.into_body().collect().await {
+            tracing::error!("failed to read payload from client (request): {err}");
+        }
+        Ok::<_, Infallible>(body_content.into_response())
+    };
+
+    let http_service = (
+        TraceLayer::new_for_http(),
+        CompressionLayer::new(),
+        AddRequiredResponseHeadersLayer::default()
+            .with_server_header_value(HeaderValue::from_static("foo")),
+        SetResponseHeaderLayer::if_not_present(
+            HeaderName::from_static("res-header"),
+            HeaderValue::from_static("res-bar"),
+        ),
+        CorsLayer::permissive(),
+    )
+        .layer(WebService::default().with_post(get_endpoint_for_size(params.server), handler));
+
+    let listener = std::net::TcpListener::bind(SocketAddress::local_ipv4(0).into_std()).unwrap();
+    let addr = listener.local_addr().unwrap();
+
+    tracing::info!(
+        "{:?} server running in multi-thread runtime at {addr}",
+        params.version
+    );
+
+    let ready = Arc::new(AtomicBool::new(false));
+    let ready_worker = ready.clone();
+
+    std::thread::spawn(move || {
+        let rt = tokio::runtime::Builder::new_multi_thread()
+            .worker_threads(4)
+            .enable_all()
+            .build()
+            .unwrap();
+        rt.block_on(async move {
+            let async_listener =
+                TcpListener::try_from_std_tcp_listener(listener, Executor::default()).unwrap();
+
+            ready_worker.store(true, Ordering::Release);
+
+            match params.version {
+                HttpVersion::Http1 => {
+                    async_listener
+                        .serve(HttpServer::http1(Executor::default()).service(http_service))
+                        .await
+                }
+                HttpVersion::Http2 => {
+                    async_listener
+                        .serve(HttpServer::h2(Executor::default()).service(http_service))
+                        .await
+                }
+            }
+        });
+    });
+
+    while !ready.load(Ordering::Acquire) {
+        std::thread::yield_now();
+    }
+
+    addr.into()
+}
+
+async fn request_payload(
+    client: impl Service<Request, Output = Response, Error = BoxError>,
+    params: TestParameters,
+    body_content: Vec<u8>,
+    address: SocketAddress,
+) {
+    let endpoint = get_endpoint_for_size(params.server);
+
+    let result = client
+        .post(format!("http://{address}/{endpoint}"))
+        .version(match params.version {
+            HttpVersion::Http1 => Version::HTTP_11,
+            HttpVersion::Http2 => Version::HTTP_2,
+        })
+        .body(body_content)
+        .send()
+        .await;
+
+    match result {
+        Ok(resp) => {
+            if let Err(err) = resp.into_body().collect().await {
+                tracing::error!("failed to recv response payload: {err}")
+            }
+        }
+        Err(err) => tracing::error!("failed to recv response: {err}"),
+    }
+}
+
+const SAMPLE_COUNT: u32 = 1000;
+
+#[divan::bench(sample_count = SAMPLE_COUNT, args = [
+    // http1
+    TestParameters { version: HttpVersion::Http1, server: Size::Small, client: Size::Small },
+    TestParameters { version: HttpVersion::Http1, server: Size::Large, client: Size::Small },
+    TestParameters { version: HttpVersion::Http1, server: Size::Small, client: Size::Large },
+    TestParameters { version: HttpVersion::Http1, server: Size::Large, client: Size::Large },
+    // http2 (h2)
+    TestParameters { version: HttpVersion::Http2, server: Size::Small, client: Size::Small },
+    TestParameters { version: HttpVersion::Http2, server: Size::Large, client: Size::Small },
+    TestParameters { version: HttpVersion::Http2, server: Size::Small, client: Size::Large },
+    TestParameters { version: HttpVersion::Http2, server: Size::Large, client: Size::Large },
+])]
+fn h1_client_server(bencher: divan::Bencher, params: TestParameters) {
+    let mut rng = rand::rng();
+
+    let server_random_bytes = random_bytes_by_size(&mut rng, params.server);
+    let address = spawn_http_server(params, server_random_bytes);
+
+    bencher
+        .with_inputs(|| {
+            let client_random_bytes = random_bytes_by_size(&mut rng, params.client);
+
+            let client = (
+                MapResponseBodyLayer::new(Body::new),
+                TraceLayer::new_for_http(),
+                DecompressionLayer::new(),
+                AddRequiredRequestHeadersLayer::default(),
+                SetRequestHeaderLayer::if_not_present(
+                    HeaderName::from_static("req-header"),
+                    HeaderValue::from_static("req-bar"),
+                ),
+            )
+                .into_layer(EasyHttpWebClient::default());
+
+            let rt = tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .unwrap();
+
+            (client, rt, client_random_bytes)
+        })
+        .bench_local_values(|(client, rt, body_content)| {
+            rt.block_on(request_payload(client, params, body_content, address))
+        });
+}
---
+++ benches/e2e_utils.rs
@@ -0,0 +1,36 @@
+use std::{
+    fs::{create_dir_all, exists, remove_file},
+    path::Path,
+};
+
+use rama::telemetry::tracing::{
+    self,
+    appender::{self},
+    subscriber::filter,
+};
+
+pub fn setup_tracing(test_file: &str) -> appender::non_blocking::WorkerGuard {
+    let e2e_test_dir = "e2e-test-logs";
+    let log_file = format!("{test_file}.log");
+    create_dir_all(e2e_test_dir).unwrap();
+
+    println!("Tracing will be piped to {e2e_test_dir}/{log_file}");
+    let log_file_path = Path::new(e2e_test_dir).join(log_file.clone());
+    if exists(&log_file_path).unwrap() {
+        remove_file(&log_file_path).unwrap();
+    }
+
+    let file_appender = appender::rolling::never(e2e_test_dir, log_file);
+    let (non_blocking, _guard) = appender::non_blocking(file_appender);
+
+    tracing::subscriber::fmt()
+        .json()
+        .with_max_level(filter::LevelFilter::DEBUG)
+        .with_target(true)
+        .with_current_span(true)
+        .with_span_list(true)
+        .with_writer(non_blocking)
+        .init();
+
+    _guard
+}
