8feb745dc84fb63d21f27d5fcfc24acee890c18b
M-Kusumgar <98405247+M-Kusumgar@users.noreply.github.com>
2026-02-03 10:39:04 +0000

    Issue 374/Benchmark for e2e h1 client server interaction (#766)
    PR https://github.com/plabayo/rama/pull/766

---
+++ benches/e2e_h1_client_server.rs
@@ -0,0 +1,169 @@
+use std::{thread, time::Duration};
+
+use rama::{
+    Layer, Service,
+    http::{
+        Body, HeaderName, HeaderValue, Request,
+        client::EasyHttpWebClient,
+        layer::{
+            compression::CompressionLayer, decompression::DecompressionLayer, trace::TraceLayer,
+        },
+        layer::{
+            cors::CorsLayer,
+            required_header::{AddRequiredRequestHeadersLayer, AddRequiredResponseHeadersLayer},
+            set_header::{SetRequestHeaderLayer, SetResponseHeaderLayer},
+        },
+        server::HttpServer,
+        service::web::WebService,
+    },
+    net::address::SocketAddress,
+    rt::Executor,
+};
+
+use rand::prelude::*;
+use tokio_test::block_on;
+
+pub mod e2e_utils;
+
+const ADDRESS: SocketAddress = SocketAddress::local_ipv4(62004);
+
+// uncomment to enable allocator profiler
+// #[global_allocator]
+// static ALLOC: divan::AllocProfiler = divan::AllocProfiler::system();
+
+fn main() {
+    // Spawn thread with explicit stack size
+    let large_stack_thread = thread::Builder::new()
+        .stack_size(1024 * 1024 * 30) // 30MB
+        .spawn(|| {
+            // Make Tokio available for the duration of the process:
+            let executor = tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .thread_stack_size(1024 * 1024 * 15) // 15MB
+                .build()
+                .unwrap();
+            let _guard = executor.enter();
+
+            // non_blocking trace appender guard needs to live for the
+            // duration of the process
+            let _appender_guard = e2e_utils::setup_tracing("e2e_h1_client_server");
+
+            // Run registered benchmarks.
+            divan::main();
+        })
+        .unwrap();
+
+    // Wait for thread to join
+    large_stack_thread.join().unwrap();
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Size {
+    Small,
+    Large,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Payload {
+    server: Size,
+    client: Size,
+}
+
+fn random_bytes_by_size(rng: &mut ThreadRng, size: Size) -> Vec<u8> {
+    match size {
+        Size::Small => {
+            let mut bytes = [0u8; 10_000];
+            rng.fill_bytes(&mut bytes);
+            bytes.to_vec()
+        }
+        Size::Large => {
+            let mut bytes = [0u8; 10_000_000];
+            rng.fill_bytes(&mut bytes);
+            bytes.to_vec()
+        }
+    }
+}
+
+fn get_endpoint_for_size(size: Size) -> &'static str {
+    match size {
+        Size::Small => "small",
+        Size::Large => "large",
+    }
+}
+
+async fn run_server(size: Size, body_content: Vec<u8>) {
+    let http_service = (
+        TraceLayer::new_for_http(),
+        CompressionLayer::new(),
+        AddRequiredResponseHeadersLayer::default()
+            .with_server_header_value(HeaderValue::from_static("foo")),
+        SetResponseHeaderLayer::if_not_present(
+            HeaderName::from_static("res-header"),
+            HeaderValue::from_static("res-bar"),
+        ),
+        CorsLayer::permissive(),
+    )
+        .layer(WebService::default().with_post(get_endpoint_for_size(size), body_content));
+
+    HttpServer::http1(Executor::default())
+        .listen(ADDRESS, http_service)
+        .await
+        .unwrap();
+}
+
+async fn request_payload(client: impl Service<Request>, payload: &Payload, body_content: Vec<u8>) {
+    let endpoint = get_endpoint_for_size(payload.server);
+
+    let req = Request::builder()
+        .uri(format!("http://{ADDRESS}/{endpoint}"))
+        .method("POST")
+        .body(Body::from(body_content))
+        .unwrap();
+
+    let _ = client.serve(req).await;
+}
+
+const SAMPLE_COUNT: u32 = 1000;
+
+#[divan::bench(sample_count = SAMPLE_COUNT, args = [
+    Payload { server: Size::Small, client: Size::Small },
+    Payload { server: Size::Large, client: Size::Small },
+    Payload { server: Size::Small, client: Size::Large },
+    Payload { server: Size::Large, client: Size::Large },
+])]
+fn h1_client_server(bencher: divan::Bencher, payload: Payload) {
+    let mut server_thread = tokio::spawn(run_server(payload.server, vec![]));
+    block_on(tokio::time::sleep(Duration::from_micros(5)));
+
+    bencher
+        .with_inputs(|| {
+            server_thread.abort();
+            block_on(tokio::time::sleep(Duration::from_micros(5)));
+
+            let mut rng = rand::rng();
+            let server_random_bytes = random_bytes_by_size(&mut rng, payload.server);
+            let client_random_bytes = random_bytes_by_size(&mut rng, payload.client);
+
+            server_thread = tokio::spawn(run_server(payload.server, server_random_bytes));
+            block_on(tokio::time::sleep(Duration::from_micros(5)));
+
+            let client = (
+                TraceLayer::new_for_http(),
+                DecompressionLayer::new(),
+                AddRequiredRequestHeadersLayer::default()
+                    .with_user_agent_header_value(HeaderValue::from_static("chrome")),
+                SetRequestHeaderLayer::if_not_present(
+                    HeaderName::from_static("req-header"),
+                    HeaderValue::from_static("req-bar"),
+                ),
+            )
+                .into_layer(EasyHttpWebClient::default());
+
+            (client, client_random_bytes)
+        })
+        .bench_local_values(|(client, body_content)| {
+            block_on(request_payload(client, &payload, body_content))
+        });
+
+    server_thread.abort();
+}
---
+++ benches/e2e_utils.rs
@@ -0,0 +1,33 @@
+use std::{
+    fs::{create_dir, exists, remove_file},
+    path::Path,
+};
+
+use rama::telemetry::tracing::{
+    self,
+    subscriber::{filter, fmt, layer::SubscriberExt, util::SubscriberInitExt},
+};
+use tracing_appender::non_blocking::WorkerGuard;
+
+pub fn setup_tracing(test_file: &str) -> WorkerGuard {
+    let e2e_test_dir = "e2e-test-logs";
+    let log_file = format!("{test_file}.log");
+    if !exists(e2e_test_dir).unwrap() {
+        create_dir(e2e_test_dir).unwrap();
+    }
+    println!("Tracing will be piped to {e2e_test_dir}/{log_file}");
+    let log_file_path = Path::new(e2e_test_dir).join(log_file.clone());
+    if exists(&log_file_path).unwrap() {
+        remove_file(&log_file_path).unwrap();
+    }
+
+    let file_appender = tracing_appender::rolling::never(e2e_test_dir, log_file);
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    tracing::subscriber::registry()
+        .with(filter::LevelFilter::DEBUG)
+        .with(fmt::layer().with_writer(non_blocking))
+        .init();
+
+    _guard
+}
