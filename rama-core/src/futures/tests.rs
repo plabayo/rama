6f70fe922cbe1d0b68a96a1708a016f74456f8e1
glendc <glen@plabayo.tech>
2026-03-06 19:09:28 +0100

    add tests for GracefulStream

--- rama-core/src/futures/tests.rs
+++ rama-core/src/futures/tests.rs
@@ -1,4 +1,4 @@
-use super::{DelayStream, FutureExt as _, StreamExt as _, stream};
+use super::{DelayStream, FutureExt as _, GracefulStream, StreamExt as _, stream};
 
 use std::time::Duration;
 use tokio::time;
@@ -27,4 +27,38 @@ async fn immediate_when_duration_zero() {
     assert_eq!(s.next().now_or_never().unwrap(), Some(10));
     assert_eq!(s.next().now_or_never().unwrap(), Some(20));
     assert_eq!(s.next().now_or_never().unwrap(), None);
+}
+
+#[tokio::test]
+async fn graceful_stream_cancels_pending_stream() {
+    time::pause();
+
+    let (tx, rx) = tokio::sync::oneshot::channel::<()>();
+    let cancel = async move {
+        let _ = rx.await;
+    };
+
+    let delayed = DelayStream::new(Duration::from_secs(10), stream::iter([1u8, 2, 3]));
+    let mut s = std::pin::pin!(GracefulStream::new(cancel, delayed));
+
+    tokio::time::sleep(Duration::from_millis(1)).await;
+    assert_eq!(s.next().now_or_never(), None);
+
+    let _ = tx.send(());
+    assert_eq!(s.next().await, None);
+    assert_eq!(s.next().await, None);
+}
+
+#[tokio::test]
+async fn graceful_stream_stops_after_cancel_even_if_stream_has_more_items() {
+    let (tx, rx) = tokio::sync::oneshot::channel::<()>();
+    let cancel = async move {
+        let _ = rx.await;
+    };
+    let mut s = std::pin::pin!(GracefulStream::new(cancel, stream::iter([1u8, 2, 3])));
+
+    assert_eq!(s.next().await, Some(1));
+    let _ = tx.send(());
+    assert_eq!(s.next().await, None);
+    assert_eq!(s.next().await, None);
 }
