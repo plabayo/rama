997a5dd00e683d5566a3775b76acfec600d166c9
glendc <glen@plabayo.tech>
2026-05-08 19:55:30 +0200

    fix unstable feature in rama-http-core

--- rama-http-core/src/lib.rs
+++ rama-http-core/src/lib.rs
@@ -26,6 +26,7 @@
 #![doc(html_logo_url = "https://raw.githubusercontent.com/plabayo/rama/main/docs/img/old_logo.png")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(test, allow(clippy::float_cmp))]
+#![cfg_attr(feature = "unstable", expect(clippy::allow_attributes))]
 #![allow(unreachable_pub)]
 #![expect(
     clippy::panic,
