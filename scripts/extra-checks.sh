fc77709487407c43722f197e778b20d5914fbdb1
Glen De Cauwsemaecker <contact@glendc.com>
2026-05-29 20:00:44 +0200

    Update scripts/extra-checks.sh

--- scripts/extra-checks.sh
+++ scripts/extra-checks.sh
@@ -7,7 +7,6 @@ exit_code=0
 # Make sure all examples are included in the rama book
 for example in $(cd $SCRIPT_DIR/.. && find examples -maxdepth 1 -type f -name '*.rs' -not -name 'mod.rs'); do
     echo "Checking $example..."
- 
     if ! grep -qr "$example" docs/book; then
         echo "❌ Example $example, missing in rama book"
         exit_code=1
