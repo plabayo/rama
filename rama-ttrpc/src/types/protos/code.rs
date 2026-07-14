0162b0f283906ec027c8ddb9623b78f965e4dcf4
brecht stamper <stamper.brecht@gmail.com>
2026-07-14 15:39:11 +0200

    remove http mapping docs

--- rama-ttrpc/src/types/protos/code.rs
+++ rama-ttrpc/src/types/protos/code.rs
@@ -10,13 +10,9 @@
 #[repr(i32)]
 pub enum Code {
     /// Not an error; returned on success
-    ///
-    /// HTTP Mapping: 200 OK
     Ok = 0,
 
     /// The operation was cancelled, typically by the caller.
-    ///
-    /// HTTP Mapping: 499 Client Closed Request
     Cancelled = 1,
 
     /// Unknown error.  For example, this error may be returned when
@@ -24,16 +20,12 @@ pub enum Code {
     /// an error space that is not known in this address space.  Also
     /// errors raised by APIs that do not return enough error information
     /// may be converted to this error.
-    ///
-    /// HTTP Mapping: 500 Internal Server Error
     Unknown = 2,
 
     /// The client specified an invalid argument.  Note that this differs
     /// from `FAILED_PRECONDITION`.  `INVALID_ARGUMENT` indicates arguments
     /// that are problematic regardless of the state of the system
     /// (e.g., a malformed file name).
-    ///
-    /// HTTP Mapping: 400 Bad Request
     InvalidArgument = 3,
 
     /// The deadline expired before the operation could complete. For operations
@@ -41,8 +33,6 @@ pub enum Code {
     /// even if the operation has completed successfully.  For example, a
     /// successful response from a server could have been delayed long
     /// enough for the deadline to expire.
-    ///
-    /// HTTP Mapping: 504 Gateway Timeout
     DeadlineExceeded = 4,
 
     /// Some requested entity (e.g., file or directory) was not found.
@@ -52,14 +42,10 @@ pub enum Code {
     /// `NOT_FOUND` may be used. If a request is denied for some users within
     /// a class of users, such as user-based access control, `PERMISSION_DENIED`
     /// must be used.
-    ///
-    /// HTTP Mapping: 404 Not Found
     NotFound = 5,
 
     /// The entity that a client attempted to create (e.g., file or directory)
     /// already exists.
-    ///
-    /// HTTP Mapping: 409 Conflict
     AlreadyExists = 6,
 
     /// The caller does not have permission to execute the specified
@@ -70,20 +56,14 @@ pub enum Code {
     /// instead for those errors). This error code does not imply the
     /// request is valid or the requested entity exists or satisfies
     /// other pre-conditions.
-    ///
-    /// HTTP Mapping: 403 Forbidden
     PermissionDenied = 7,
 
     /// The request does not have valid authentication credentials for the
     /// operation.
-    ///
-    /// HTTP Mapping: 401 Unauthorized
     Unauthenticated = 16,
 
     /// Some resource has been exhausted, perhaps a per-user quota, or
     /// perhaps the entire file system is out of space.
-    ///
-    /// HTTP Mapping: 429 Too Many Requests
     ResourceExhausted = 8,
 
     /// The operation was rejected because the system is not in a state
@@ -102,8 +82,6 @@ pub enum Code {
     ///       fails because the directory is non-empty, `FAILED_PRECONDITION`
     ///       should be returned since the client should not retry unless
     ///       the files are deleted from the directory.
-    ///
-    /// HTTP Mapping: 400 Bad Request
     FailedPrecondition = 9,
 
     /// The operation was aborted, typically due to a concurrency issue such as
@@ -111,8 +89,6 @@ pub enum Code {
     ///
     /// See the guidelines above for deciding between `FAILED_PRECONDITION`,
     /// `ABORTED`, and `UNAVAILABLE`.
-    ///
-    /// HTTP Mapping: 409 Conflict
     Aborted = 10,
 
     /// The operation was attempted past the valid range.  E.g., seeking or
@@ -130,21 +106,15 @@ pub enum Code {
     /// error) when it applies so that callers who are iterating through
     /// a space can easily look for an `OUT_OF_RANGE` error to detect when
     /// they are done.
-    ///
-    /// HTTP Mapping: 400 Bad Request
     OutOfRange = 11,
 
     /// The operation is not implemented or is not supported/enabled in this
     /// service.
-    ///
-    /// HTTP Mapping: 501 Not Implemented
     Unimplemented = 12,
 
     /// Internal errors.  This means that some invariants expected by the
     /// underlying system have been broken.  This error code is reserved
     /// for serious errors.
-    ///
-    /// HTTP Mapping: 500 Internal Server Error
     Internal = 13,
 
     /// The service is currently unavailable.  This is most likely a
@@ -153,13 +123,9 @@ pub enum Code {
     ///
     /// See the guidelines above for deciding between `FAILED_PRECONDITION`,
     /// `ABORTED`, and `UNAVAILABLE`.
-    ///
-    /// HTTP Mapping: 503 Service Unavailable
     Unavailable = 14,
 
     /// Unrecoverable data loss or corruption.
-    ///
-    /// HTTP Mapping: 500 Internal Server Error
     DataLoss = 15,
 }
 
